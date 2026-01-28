27aabea563600cf52caf2a29e95f2d8b9ca31b37
SyedSameen <sameen12216100@gmail.com>
2026-01-28 16:32:26 +0600

    major changes

--- src/app/survey/[id]/layout.tsx
+++
@@ -1,5 +0,0 @@
-import { ReactNode } from "react";
-
-export default function SurveyLayout({ children }: { children: ReactNode }) {
-  return <div className="dark min-h-screen bg-background">{children}</div>;
-}
